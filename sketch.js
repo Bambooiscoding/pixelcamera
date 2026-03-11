9f375e2a61c0b59ed2fe7e810d06448a95e67e3d
Hanyuan Zhu <hzhu18@sva.edu>
2026-03-10 22:39:40 -0400

    Modify emoji array and change text size
    Updated emoji array and adjusted text size for display.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let cam;
 let shutterBtn;
-let emo = ["🌑","🌒","🌓","🌓","🌔","🌕","🌖","🌗","🌗","🌘","🌑"];
+let emo = ["🌑","🌑","🌑","🌑","🌒","🌒","🌒","🌓","🌓","🌔","🌕","🌖","🌗","🌘"];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -28,7 +28,7 @@ function draw() {
       let b = cam.pixels[p+2];
       let br = (r + g + b)/3;
       let index = floor(map(br, 0, 255, 0, (emo.length-1)));
-      textSize(14);
+      textSize(12);
       text(emo[index],x,y);
       }
    }
